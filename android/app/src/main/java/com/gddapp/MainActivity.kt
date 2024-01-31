f40362c849e9ba6f2c973ccb0a6f3d6f85bc8ad6
Nick Dowsett <nickdowsett42@gmail.com>
2024-01-31 08:18:26 +0800

    Added react-native-navi dependency

--- android/app/src/main/java/com/gddapp/MainActivity.kt
+++ android/app/src/main/java/com/gddapp/MainActivity.kt
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+// react-native-screens install instructions
+import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
 
@@ -12,6 +14,11 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "GddApp"
+
+  // react-native-screens install instructions
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
