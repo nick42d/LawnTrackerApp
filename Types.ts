86f52cea5c77b90770e275c56e3526a43c72eb3b
Nick Dowsett <nickdowsett42@gmail.com>
2024-02-11 15:38:40 +0800

    Added type model for weather API history

--- Types.ts
+++ Types.ts
@@ -17,3 +17,100 @@ export type GddTracker = {
 export type GddSettings = {
   low_alert_threshold_perc: number;
 };
+
+export type WeatherApiHistory = {
+  location: WeatherApiLocation;
+  forecast: WeatherApiForecast;
+};
+
+export type WeatherApiLocation = {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+};
+
+export type WeatherApiForecast = {
+  forecastday: WeatherApiForecastDay[];
+};
+export type WeatherApiForecastDay = {
+  date: string;
+  date_epoch: number;
+  day: WeatherApiDay;
+  astro: WeatherApiAstro;
+  hour: WeatherApiHour[];
+};
+export type WeatherApiDay = {
+  maxtemp_c: number;
+  maxtemp_f: number;
+  mintemp_c: number;
+  mintemp_f: number;
+  avgtemp_c: number;
+  avgtemp_f: number;
+  maxwind_mph: number;
+  maxwind_kph: number;
+  totalprecip_mm: number;
+  totalprecip_in: number;
+  avgvis_km: number;
+  avgvis_miles: number;
+  avghumidity: number;
+  daily_will_it_rain: number;
+  daily_chance_of_rain: number;
+  daily_will_it_snow: number;
+  daily_chance_of_snow: number;
+  condition: WeatherApiCondition;
+  uv: number;
+};
+export type WeatherApiAstro = {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: string;
+};
+export type WeatherApiHour = {
+  time_epoch: number;
+  time: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: WeatherApiCondition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  will_it_rain: number;
+  chance_of_rain: number;
+  will_it_snow: number;
+  chance_of_snow: number;
+  vis_km: number;
+  vis_miles: number;
+  gust_mph: number;
+  gust_kph: number;
+  uv: number;
+};
+
+export type WeatherApiCondition = {
+  text: string;
+  icon: string;
+  code: number;
+};
