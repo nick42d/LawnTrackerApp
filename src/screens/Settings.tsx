8c4aac89929ab4f76f6b5138e6e8514a7e8e5c5e
Nick Dowsett <nickdowsett42@gmail.com>
2024-03-12 07:30:24 +0800

    Added generic Dialog component

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -89,32 +89,48 @@ export default function SettingsScreen({
         />
       </List.Section>
       <Portal>
-        <BaseTempDialog
+        <GenericSelectionDialog<number>
           visible={tempDialogVisible}
           setVisible={x => {
             setTempDialogVisible(x);
           }}
+          curValue={1}
+          setCurValue={(set: number) => {}}
+          values={[
+            {label: '123', value: 1},
+            {label: '1234', value: 2},
+          ]}
         />
       </Portal>
     </ScrollView>
   );
 }
 
-function BaseTempDialog(props: {
+function GenericSelectionDialog<T>(props: {
   visible: boolean;
   setVisible: (set: boolean) => void;
+  values: {label: string; value: T}[];
+  curValue: T;
+  setCurValue: (set: T) => void;
 }): React.JSX.Element {
   return (
     <Dialog visible={props.visible} onDismiss={() => props.setVisible(false)}>
       <Dialog.Title>Select Base Temp</Dialog.Title>
       <Dialog.Content>
         <RadioButton.Group
-          value="second"
+          // All types in JavaSCript can be converted to a string, so this is safe.
+          value={String(props.curValue)}
           onValueChange={val =>
             console.log(`Radio button pressed, ${JSON.stringify(val)}`)
           }>
-          <RadioButton.Item label="111" value="first" status="checked" />
-          <RadioButton.Item label="111" value="second" status="checked" />
+          {props.values.map(value => {
+            return (
+              <RadioButton.Item
+                label={value.label}
+                value={String(value.value)}
+              />
+            );
+          })}
         </RadioButton.Group>
       </Dialog.Content>
       <Dialog.Actions>
