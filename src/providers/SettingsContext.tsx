ab6c4a751b1e8ec7060648accb1015e74365ac3b
Nick Dowsett <nickdowsett42@gmail.com>
2024-03-15 07:11:44 +0800

    Begin implementation of local storage

--- src/providers/SettingsContext.tsx
+++ src/providers/SettingsContext.tsx
@@ -1,6 +1,7 @@
 import {createContext, useEffect, useState} from 'react';
 import {defaultSettings as defaultSettings} from '../Mock';
 import {Settings, SettingsState} from '../state/State';
+import {storage} from '../App';
 
 export const SettingsContext = createContext<SettingsState>({
   settings: defaultSettings(),
@@ -9,8 +10,20 @@ export const SettingsContext = createContext<SettingsState>({
 
 export function SettingsContextProvider({children}: React.PropsWithChildren) {
   const [settings, setSettings] = useState(defaultSettings());
+  useEffect(() => {
+    console.log('Settings context loaded');
+    const test = storage.getBoolean('settingset');
+    if (test) console.log('Settings storage variable set');
+    else console.log('Settings storage variable not set');
+  }, []);
+  function setSettingsWrapper(settingTemp: Settings) {
+    console.log('Setting settings');
+    storage.set('settingset', true);
+    setSettings(settingTemp);
+  }
   return (
-    <SettingsContext.Provider value={{settings, setSettings}}>
+    <SettingsContext.Provider
+      value={{settings, setSettings: setSettingsWrapper}}>
       {children}
     </SettingsContext.Provider>
   );
