40de9e5372a69d33c9796401060a0e4d25a0ed07
Nick Dowsett <nickdowsett42@gmail.com>
2024-03-21 17:56:24 +0800

    Implment images and descriptions for new api

--- src/components/LocationsCard.tsx
+++ src/components/LocationsCard.tsx
@@ -5,7 +5,10 @@ import * as central_styles from '../Styles';
 import {HomeLocationsTabScreenProps} from '../navigation/Root';
 import {Location} from '../state/State';
 import {useContext} from 'react';
-import {WEATHER_IMAGES} from '../../assets/weather_icons/WeatherImages';
+import {
+  WEATHER_IMAGES,
+  WeatherImagesObject,
+} from '../../assets/weather_icons/WeatherImages';
 import {StateContext} from '../providers/StateContext';
 import {WeatherAppCondition} from '../api/Types';
 
@@ -21,13 +24,19 @@ export function LocationsCard({location, navigation}: LocationsCardProps) {
     // TODO: Check that no GddCards use this location
     if (deleteLocationName !== undefined) deleteLocationName(location.name);
   }
-  // TODO: Add day/night
-  function getWeatherIcon(condition: WeatherAppCondition): any {
-    return WEATHER_IMAGES.find(x => x.code === condition.code)?.daySrc;
+  // TODO: Type checking
+  function getWeatherImagesObject(condition: WeatherAppCondition) {
+    const weatherImageSrc = WEATHER_IMAGES.find(x => x.code === condition.code);
+    if (weatherImageSrc === undefined) return undefined;
+    if (condition.isDay) {
+      return {img: weatherImageSrc.daySrc, desc: weatherImageSrc.descDay};
+    } else {
+      return {img: weatherImageSrc.daySrc, desc: weatherImageSrc.descDay};
+    }
   }
-  const weatherIconCode = location.weather
-    ? getWeatherIcon(location.weather.current_condition)
-    : 0;
+  const weatherImages = location.weather
+    ? getWeatherImagesObject(location.weather.current_condition)
+    : undefined;
   return (
     <Card
       mode="elevated"
@@ -38,12 +47,12 @@ export function LocationsCard({location, navigation}: LocationsCardProps) {
       }}>
       <Card.Title
         title={location.name}
-        subtitle={location.weather?.current_condition.code.toString()}
+        subtitle={weatherImages?.desc}
         titleVariant={CARD_TITLE_VARIANT}
         left={() => (
           <Text>{location.weather?.current_condition.temp.toString()}°</Text>
         )}
-        // right={() => <Image source={weatherIconCode} />}
+        right={() => <Image source={weatherImages?.img} />}
       />
       <Card.Content>
         <Text>
