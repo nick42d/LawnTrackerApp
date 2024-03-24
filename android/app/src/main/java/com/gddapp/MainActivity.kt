f0982639a0f30ab8e4bf943836bf3aa3bc37dd74
Nick Dowsett <nickdowsett42@gmail.com>
2024-03-25 07:05:35 +0800

    Renamed app

--- android/app/src/main/java/com/gddapp/MainActivity.kt
+++ android/app/src/main/java/com/gddapp/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "GddApp"
+  override fun getMainComponentName(): String = "Lawn Tracker"
 
   // react-native-screens install instructions
   override fun onCreate(savedInstanceState: Bundle?) {
