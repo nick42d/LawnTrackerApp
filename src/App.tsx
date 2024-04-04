ccb91f8d089b93f4a2c29ff031ced45119f6d367
Nick Dowsett <nickdowsett42@gmail.com>
2024-04-04 21:50:22 +0800

    Improved loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,8 @@ import {
   SettingsContextProvider,
 } from './providers/SettingsContext';
 import {StateContextProvider} from './providers/StateContext';
-import Maplibre from '@maplibre/maplibre-react-native';
 import React from 'react';
-import BackgroundFetch from 'react-native-background-fetch';
-import {onDisplayNotification} from './Notification';
+import {initBackgroundFetch} from './Worker';
 
 const {LightTheme, DarkTheme} = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
@@ -33,43 +31,8 @@ const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
 const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 export default function App(): React.JSX.Element {
-  async function initBackgroundFetch() {
-    // BackgroundFetch event handler.
-    const onEvent = async (taskId: string) => {
-      console.log('[BackgroundFetch] task: ', taskId);
-      // Do your background work...
-      // Perform an example HTTP request.
-      // Important:  await asychronous tasks when using HeadlessJS.
-      let response = await fetch('https://reactnative.dev/movies.json');
-      let responseJson = await response.json();
-      console.log('[BackgroundFetch] reponse: ', responseJson);
-      await onDisplayNotification();
-      // IMPORTANT:  You must signal to the OS that your task is complete.
-      BackgroundFetch.finish(taskId);
-    };
-
-    // Timeout callback is executed when your Task has exceeded its allowed running-time.
-    // You must stop what you're doing immediately BackgroundFetch.finish(taskId)
-    const onTimeout = async (taskId: string) => {
-      console.warn('[BackgroundFetch] TIMEOUT task: ', taskId);
-      BackgroundFetch.finish(taskId);
-    };
-
-    // Initialize BackgroundFetch only once when component mounts.
-    let status = await BackgroundFetch.configure(
-      {
-        enableHeadless: true,
-        stopOnTerminate: false,
-        startOnBoot: true,
-        minimumFetchInterval: 15,
-      },
-      onEvent,
-      onTimeout,
-    );
-
-    console.log('[BackgroundFetch] configure status: ', status);
-  }
-  // POC for background fetch
+  // TODO: Figure out where this needs to go!
+  // Initialize BackgroundFetch only once when component mounts.
   React.useEffect(() => {
     initBackgroundFetch();
   }, []);
--- src/screens/Loading.tsx
+++ src/screens/Loading.tsx
@@ -1,10 +1,36 @@
-import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Image, StyleSheet, View} from 'react-native';
+import {ActivityIndicator, Text} from 'react-native-paper';
+import ViewLocationCardScreen from './ViewLocationCard';
 
 export default function LoadingScreen() {
   return (
-    <View>
-      <Text>Loading!</Text>
+    <View style={loadScreenStyles.container}>
+      <Text variant="displayMedium">LawnTracker</Text>
+      <Image
+        style={loadScreenStyles.logo}
+        source={require('../../android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png')}
+      />
+      <View style={loadScreenStyles.loadingText}>
+        <Text variant="displaySmall">Loading</Text>
+        <ActivityIndicator size="small" />
+      </View>
     </View>
   );
 }
+
+const loadScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
